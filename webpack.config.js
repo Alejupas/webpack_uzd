ab3386744f33dd1f639cfc35ba0ccea86f887ae7
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-24 23:45:58 +0300

    major upd

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,42 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: {},
-  output: {},
-  plugins: [],
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+    assetModuleFilename: "images/[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "VILNIAUS RYTAS",
+      template: "src/template.html",
+    }),
+  ],
 };
